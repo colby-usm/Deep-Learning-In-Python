ed815257e330a07464465c39b67fc08fca2d751d
Colby Wirth <wirthcolby00@gmail.com>
2026-05-18 13:57:24 -0400

    continuing work on Tensor class

--- neural/LossFunctions.py
+++ neural/LossFunctions.py
@@ -1,4 +1,3 @@
-import numpy as np
 from neural.Tensor import Tensor
 
 class SimpleCrossEntropy:
--- neural/Tensor.py
+++ neural/Tensor.py
@@ -1,84 +1,97 @@
 import numpy as np
 
-class Tensor():
+
+class Tensor:
     def __init__(
         self,
         data: float | int | np.floating | np.ndarray | None = None,
         shape: tuple[int, ...] | None = None,
         dtype=np.float32,
         requires_grad: bool = True,
-        parents = None
+        parents=None,
     ):
 
-        assert not (data is not None and shape is not None), "Provide either data or shape, not both."
-        assert not (data is None and shape is None), "Must provide either data or shape."
+        assert not (data is not None and shape is not None), (
+            "Provide either data or shape, not both."
+        )
+        assert not (data is None and shape is None), (
+            "Must provide either data or shape."
+        )
 
         self.dtype = dtype
 
         self.requires_grad = requires_grad
-        self.grad = None 
-        self.parents =parents
+        self.grad = None
+        self.parents = parents
         self.grad_fn = None
 
         if data is not None:
             self.data = np.atleast_1d(np.array(data, dtype=self.dtype))
         else:
             self.data = np.array(
-                np.random.default_rng().random(shape),
-                dtype=self.dtype
+                np.random.default_rng().random(shape), dtype=self.dtype
             )
 
         self.shape = self.data.shape
 
-
     def __repr__(self):
-            return f"Tensor(data={self.data}, shape={self.shape}, dtype={self.dtype}, requires_grad={self.requires_grad})"
+        return f"Tensor(data={self.data}, shape={self.shape}, dtype={self.dtype}, requires_grad={self.requires_grad})"
 
+    # TODO: implement
+    def _backward(self):
+        pass
 
     def __add__(self, other):
-        other = other if isinstance(other, Tensor) else Tensor(other, requires_grad=False)
-        t = Tensor(self.data + other.data,
-                   dtype=self.dtype,
-                   requires_grad=self.requires_grad or other.requires_grad,
-                   parents=[self,other]
+        other = (
+            other if isinstance(other, Tensor) else Tensor(other, requires_grad=False)
+        )
+        t = Tensor(
+            self.data + other.data,
+            dtype=self.dtype,
+            requires_grad=self.requires_grad or other.requires_grad,
+            parents=[self, other],
         )
 
-
-        #TODO: implement grad_fn
+        # TODO: implement grad_fn
         def grad_fn(grad):
-            pass
+            if self.requred_grad:
+                self._backward(grad)
+            if other.requires_grad:
+                other._backward(grad)
 
         t.grad_fn = grad_fn
         return t
-
 
     def __sub__(self, other):
-        other = other if isinstance(other, Tensor) else Tensor(other, requires_grad=False)
-        t = Tensor(self.data - other.data,
-                   dtype=self.dtype,
-                   requires_grad=self.requires_grad or other.requires_grad,
-                   parents=[self,other]
+        other = (
+            other if isinstance(other, Tensor) else Tensor(other, requires_grad=False)
+        )
+        t = Tensor(
+            self.data - other.data,
+            dtype=self.dtype,
+            requires_grad=self.requires_grad or other.requires_grad,
+            parents=[self, other],
         )
 
-
-        #TODO: implement grad_fn
+        # TODO: implement grad_fn
         def grad_fn(grad):
             pass
 
         t.grad_fn = grad_fn
         return t
 
-
     def __mul__(self, other):
-        other = other if isinstance(other, Tensor) else Tensor(other, requires_grad=False)
-        t = Tensor(self.data * other.data,
-                   dtype=self.dtype,
-                   requires_grad=self.requires_grad or other.requires_grad,
-                   parents=[self,other]
+        other = (
+            other if isinstance(other, Tensor) else Tensor(other, requires_grad=False)
+        )
+        t = Tensor(
+            self.data * other.data,
+            dtype=self.dtype,
+            requires_grad=self.requires_grad or other.requires_grad,
+            parents=[self, other],
         )
 
-
-        #TODO: implement grad_fn
+        # TODO: implement grad_fn
         def grad_fn(grad):
             pass
 
@@ -88,66 +101,97 @@ class Tensor():
     def __rmul__(self, other):
         return self.__mul__(other)
 
+    # TODO: implement
+    def __truediv__(self, other):
+        pass
+
+    # TODO: implement
+    def __rtruediv__(self, other):
+        pass
+
+    # TODO: implement
+    def __neg__(self):
+        pass
+
+    # TODO: implement
+    def __pow__(self, exponent):
+        pass
+
+    # TODO: implement
+    def max(self, axis=None):
+        pass
+
+    # TODO: implement
+    def min(self, axis=None):
+        pass
+
+    # TODO: implement
+    def mean(self, axis=None, keepdims=False):
+        pass
+
+    # TODO: implement
+    def exp(self):
+        pass
 
     def log(self):
-        t = Tensor(np.log(self.data),
-                   dtype=self.dtype,
-                   requires_grad=self.requires_grad,
-                   parents=[self]
+        t = Tensor(
+            np.log(self.data),
+            dtype=self.dtype,
+            requires_grad=self.requires_grad,
+            parents=[self],
         )
 
-
-        #TODO implement grad_fn
+        # TODO implement grad_fn
         def grad_fn(grad):
             pass
 
         t.grad_fn = grad_fn
         return t
-
 
     def dot(self, other):
         return self.__matmul__(other)
 
-
     def __matmul__(self, other):
         other = other if isinstance(other, Tensor) else Tensor(other)
-        t = Tensor(np.dot(self.data, other.data),
-                   dtype=self.dtype,
-                   requires_grad=self.requires_grad or other.requires_grad,
-                   parents=[self, other]
+        t = Tensor(
+            np.dot(self.data, other.data),
+            dtype=self.dtype,
+            requires_grad=self.requires_grad or other.requires_grad,
+            parents=[self, other],
         )
 
-        #TODO: implement grad_fn
+        # TODO: implement grad_fn
         def grad_fn(grad):
             pass
 
         t.grad_fn = grad_fn
         return t
-
 
     def sum(self):
-        t = Tensor(np.sum(self.data),
-                   dtype=self.dtype,
-                   requires_grad=self.requires_grad,
-                   parents=[self]
-                   )
+        t = Tensor(
+            np.sum(self.data),
+            dtype=self.dtype,
+            requires_grad=self.requires_grad,
+            parents=[self],
+        )
 
-        #TODO: implement grad_fn
+        # TODO: implement grad_fn
         def grad_fn(grad):
             pass
 
         t.grad_fn = grad_fn
         return t
-
 
     @property
     def T(self):
-        t = Tensor(self.data.T,
-                   dtype=self.dtype,
-                   requires_grad=self.requires_grad,
-                   parents=[self]
+        t = Tensor(
+            self.data.T,
+            dtype=self.dtype,
+            requires_grad=self.requires_grad,
+            parents=[self],
         )
 
+        # TODO: implement grad_fn
         def grad_fn(grad):
             pass
 
